5ec4106db1a9d41843d515120851872d60755f1c
Dzmitry Hrachou <falizyer@gmail.com>
2018-11-28 21:39:08 +0300

    main navigation menu

--- src/application.component.tsx
+++ src/application.component.tsx
@@ -22,6 +22,13 @@ export class ApplicationComponent extends React.PureComponent {
      * @override
      */
     public render() {
+        const navLinks = [{
+            title: "Home",
+            href: "#"
+        }, {
+            title: "Contact",
+            href: "#"
+        }];
         const slides = [{
             url: "https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg",
             title: "First",
@@ -40,35 +47,59 @@ export class ApplicationComponent extends React.PureComponent {
                 <h1>The Media Hub</h1>
                 <header>
                     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
                         <a className="navbar-brand" href="#">Navbar</a>
+
                         <button className="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02"
-                                aria-expanded="false" aria-label="Toggle navigation">
+                                data-target="#main-navigation-menu"
+                                aria-controls="main-navigation-menu" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
 
-                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <div className="collapse navbar-collapse" id="main-navigation-menu">
+                            <ul className="navbar-nav mr-auto">
                                 <li className="nav-item active">
-                                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                                    <a className="nav-link" href="#">Home
+                                        <span className="sr-only">(current)</span>
+                                    </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">Link</a>
+                                    <a className="nav-link" href="#">Features</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link disabled" href="#">Disabled</a>
+                                    <a className="nav-link" href="#">Pricing</a>
                                 </li>
+
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink"
+                                       data-toggle="dropdown" aria-haspopup="true"
+                                       aria-expanded="false">Dropdown</a>
+                                    <div className="dropdown-menu dropdown-primary"
+                                         aria-labelledby="navbarDropdownMenuLink">
+                                        <a className="dropdown-item" href="#">Action</a>
+                                        <a className="dropdown-item" href="#">Another action</a>
+                                        <a className="dropdown-item" href="#">Something else here</a>
+                                    </div>
+                                </li>
+
                             </ul>
-                            <form className="form-inline my-2 my-lg-0">
-                                <input className="form-control mr-sm-2" type="search" placeholder="Search"/>
-                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search
-                                </button>
+
+                            <form className="form-inline">
+                                <div className="md-form my-0">
+                                    <input className="form-control mr-sm-2" type="text" placeholder="Search"
+                                           aria-label="Search" />
+                                </div>
                             </form>
                         </div>
+
                     </nav>
                 </header>
                 <section className={"top-channels-carousel"}>
-                    <Carousel activeItem={1} length={slides.length} showControls={true} showIndicators={true} className="z-depth-1">
+                    <Carousel activeItem={1}
+                              length={slides.length}
+                              showControls={true}
+                              showIndicators={true}
+                              className="z-depth-1">
                         <CarouselInner>
                             {
                                 slides.map((slide, index) => {
